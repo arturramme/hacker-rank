4c77ba64518d7db2fbf703491ebb5cdc7d8a284a
Artur Ramme <arturbramme@gmail.com>
2023-02-10 15:58:24 -0300

    add lonely integer and diagonal difference exercises

---
+++ diagonalDifference.cs
@@ -0,0 +1,12 @@
+﻿//var arr = new List<List<int>> { new List<int> { 11, 2, 4 }, new List<int> { 4, 5, 6 }, new List<int> { 10, 8, -12 } };
+//var square = arr.Count;
+//var totalLeft = 0;
+//var totalRight = 0;
+
+//for (int i = 0; i < square; i++)
+//{
+//    totalLeft += arr[i][i];
+//    totalRight += arr[i][square - i - 1];
+//}
+
+//Console.WriteLine(Math.Abs(totalLeft - totalRight));
---
+++ lonelyInteger.cs
@@ -0,0 +1,56 @@
+﻿//var arr = new int[] { 1, 2, 3, 4, 3, 2, 1 };
+
+//int lonelyInteger = 0;
+
+
+////Codev2
+//Dictionary<int, int> freq = new Dictionary<int, int>();
+//foreach (int i in arr)
+//{
+//    if (freq.ContainsKey(i))
+//        freq[i]++;
+//    else
+//        freq.Add(i, 1);
+//}
+
+//foreach (var item in freq)
+//{
+//    if (item.Value == 1)
+//    {
+//        lonelyInteger = item.Key;
+//    }
+//}
+
+////Code v1
+////int lonelyInteger = 0;
+////bool alreadyTested = false;
+////var arrDuplicateIntegeger = new List<int>(arr.Length);
+////for (int i = 0; i < arr.Length; i++)
+////{
+////    for (int j = 0; j < arrDuplicateIntegeger.Count; j++)
+////    {
+////        if (arr[i] == arrDuplicateIntegeger[j])
+////        {
+////            alreadyTested = true;
+////        }
+////    }
+
+////    if (alreadyTested)
+////    {
+////        continue;
+////    }
+
+////    for (int y = 0; y < arr.Length; y++)
+////    {
+////        if (i != y && arr[i] == arr[y])
+////        {
+////            lonelyInteger = 0;
+////            arrDuplicateIntegeger.Add(arr[i]);
+////            break;
+////        }
+
+////        lonelyInteger = arr[i];
+////    }
+////}
+
+//Console.WriteLine(lonelyInteger);
